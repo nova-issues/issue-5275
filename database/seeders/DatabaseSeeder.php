9421495a279afea7fec1122379d8664f8085e1a7
Mior Muhammad Zaki <crynobone@gmail.com>
2023-02-20 13:53:30 +0800

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $this->call(UserTableSeeder::class);
+        
         $customer = Customer::create();
 
         $departure = Departure::create([
